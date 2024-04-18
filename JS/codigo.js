5030ed66fe4ca22e34bfd829eeb57da5012f72f0
TanoOliva <lukioliva@gmail.com>
2024-04-17 23:37:03 -0400

    Primer Commit Significativo
    Inicio Sesion
    Registro
    inicio
    index
    
    De las cosas iniciales, hay que arreglar la disposicion de los logos en los distintos html, y seguir utilizando la plantilla de inicio.html para continuar con las otras pantallas que tienen la misma barra de navegacion

---
+++ JS/codigo.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname === '/registro.html') {
+      // Lógica para el formulario de registro
+      var registerForm = document.getElementById('register-form');
+      registerForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evitar el envío del formulario por defecto
+        // Aquí puedes implementar la lógica para enviar los datos del formulario al servidor
+      });
+    }
+  
+    if (window.location.pathname === '/login.html') {
+      // Lógica para el formulario de inicio de sesión
+      var loginForm = document.getElementById('login-form');
+      loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evitar el envío del formulario por defecto
+        // Aquí puedes implementar la lógica para enviar los datos del formulario al servidor
+      });
+    }
+  });
+  
