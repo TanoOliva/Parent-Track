309d93ff14b4e4ce8bc5535379de73d956b5c9cc
Luciano Andrés Oliva Jara <luciano.oliva.j@mail.pucv.cl>
2024-05-24 10:21:40 -0400

    arreglo Login
    sigue siendo dificil de indicar con el puntero el correo y contraseña pero con tab funciona bien

--- ionic-react/src/pages/Login.tsx
+++ ionic-react/src/pages/Login.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonContent, IonPage, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonLabel, IonText } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
 import './Login.css';
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (email === 'admin@admin.com' && password === 'Admin') {
+      localStorage.setItem('isAuthenticated', 'true');
+      history.push('/home');
+    } else {
+      alert('Correo electrónico o contraseña incorrectos');
+    }
+  };
+
   return (
     <IonPage>
       <IonContent className="ion-padding">
         <div className="auth-container">
           <img src="src/imagenes/logo.jpg" className="logo-small" alt="Logo" />
-          
           <h2>Bienvenid@</h2>
           <p>Enter your email to sign up for this app</p>
-          <form className="auth" id="login">
+          <form className="auth" id="login" onSubmit={handleLogin}>
             <IonItem>
               <IonLabel position="floating">Correo electrónico</IonLabel>
-              <IonInput type="email" required></IonInput>
+              <IonInput type="email" required onIonChange={e => setEmail(e.detail.value!)}></IonInput>
             </IonItem>
             <IonItem>
               <IonLabel position="floating">Contraseña</IonLabel>
-              <IonInput type="password" required></IonInput>
+              <IonInput type="password" required onIonChange={e => setPassword(e.detail.value!)}></IonInput>
             </IonItem>
             <IonButton expand="block" className="button" type="submit">Iniciar sesión</IonButton>
           </form>
